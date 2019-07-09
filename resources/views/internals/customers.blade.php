4edbbfa77a095c646801a6dd4da391d3fc89d18b
Vitor Hugo <vitor.hugo@corebiz.com.br>
2019-07-09 11:54:28 -0300

    corrected configuration files, created model and inserted data to the database

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -6,7 +6,18 @@
     <ul>
         <!-- Blade syntax simplifies the '<?php ?>' -->
         @foreach ($customers as $customer)
-            <li>{{$customer}}</li>
+            <li>{{$customer->name}}</li>
         @endforeach
     </ul>
+
+    <h1>Add Customer</h1>
+
+    <form action="customers" method="POST">
+        <input type="text" placeholder="Customer Name" name="name" id="name">
+        <button type="submit">Add</button>
+        <!-- For security laravel only allows to pass data throug our form if we pass '@csrf', so laravel knows that you are really you passing data and not someone else -->
+        @csrf
+    </form>
+
+    {{$errors->first('name')}} <!-- Get the first error after returning from controller -->
 @endsection
