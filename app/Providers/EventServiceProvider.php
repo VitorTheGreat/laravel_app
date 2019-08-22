688d48d85a42c6f1b99e1b0cf4e542be57f5785b
Vitor Hugo <vitor.hugo@corebiz.com.br>
2019-08-22 20:41:45 -0300

    started event listeners class

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Events\NewCustomerHasRegisteredEvent;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -15,8 +16,15 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        // Registered::class => [
+        //     SendEmailVerificationNotification::class,
+        // ],
+
+        //For this event we have three listeners
+        NewCustomerHasRegisteredEvent::class => [ //when this event here occurs, we have to run the listeners inside it either
+            \App\Listeners\WelcomeNewCustomerListener::class,
+            \App\Listeners\RegisterCustomerToNewsletterListener::class, //If we didn't created the listeners and wrote them here, laravel automatically create them for us
+            \App\Listeners\NotifyViaSlackListener::class, //Passing the exadclty path here so it can generate the listeners for us, for that we need to run: php artisan event:generate
         ],
     ];
 
