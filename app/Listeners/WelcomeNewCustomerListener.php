2c603f62a9063039d4ce7469c6f54668b95490fb
Vitor Hugo <vitor.hugo@corebiz.com.br>
2019-08-26 20:52:10 -0300

    learned about QUEUES, the basics

--- app/Listeners/WelcomeNewCustomerListener.php
+++ app/Listeners/WelcomeNewCustomerListener.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class WelcomeNewCustomerListener
+class WelcomeNewCustomerListener implements ShouldQueue
 {
     /**
      * Create the event listener.
